c11be2c810233de3d02a8ecf58ee4d71b3618d14
KoKorte <niko.korteniemi@hotmail.com>
2022-04-22 11:07:53 +0300

    Morsecode Typer App

--- app/src/main/java/com/example/morsecodekeyboard/MainActivity.kt
+++ app/src/main/java/com/example/morsecodekeyboard/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.morsecodekeyboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//test//
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
